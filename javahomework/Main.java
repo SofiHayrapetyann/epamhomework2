445bdff7851f2c4d3520e227f1ecd46418e72669
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2022-12-22 00:19:43 +0400

    add javahomework1

---
+++ javahomework/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+     public static void main(String[] args) {
+            String x = "10";
+            int y = 20;
+            String z = x + y;
+            System.out.println(z);
+        }
+    }
+
